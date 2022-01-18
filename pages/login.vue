e5aee5001b368b5a4310aefdfc19ff8c37a976b1
Dimitri Raveleau <yuldrish@gmail.com>
2022-01-18 10:41:30 +0100

    initialisation de ma base de travaille avec nuxt et debut de travaille sur la page login

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<div class="w-full h-52 flex justify-center ">
+			<img src="@/assets/images/groupomania.svg" alt="" class="h-full">
+		</div>
+		<div class="border-2 w-full h-1 border-red-600"></div>
+		<div>
+
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {},
+}
+</script>
