82da8f209d52e5cd6b74df6adcd5a290ecba78f8
Dimitri Raveleau <yuldrish@gmail.com>
2022-02-10 11:28:32 +0100

    mise en form

--- front/pages/signUp.vue
+++ front/pages/signUp.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="h-screen">
     <Header />
-    <div class="w-full justify-center flex-col bg-white mb-10 h-3/4
-    ">
+    <div class="w-full justify-center flex-col bg-white mb-10 h-3/4">
       <form
-        class="w-4/5 mx-auto p-10 bg-[url('@/assets/images/paper-grey.webp')] bg-cover rounded-lg flex flex-col md:flex-row m-5"
+        class="
+          w-4/5
+          mx-auto
+          p-10
+          bg-[url('@/assets/images/paper-grey.webp')] bg-cover
+          rounded-lg
+          flex flex-col
+          md:flex-row
+          m-5
+        "
       >
         <div class="flex w-full p-2 gap-5 flex-col md:w-1/2">
           <label
             class="text-lg text-center font-medium text-white"
             for="departement"
             >Département<input
-              class="border-4 rounded-lg p-2 w-full border-black text-black"
+              class="
+                border-4
+                rounded-lg
+                p-2
+                w-full
+                border-black
+                text-black
+                focus:outline-none
+              "
               type="text"
               name="departement"
               placeholder="commercial"
@@ -20,7 +36,15 @@
           /></label>
           <label class="text-lg text-center font-medium text-white" for="name"
             >Nom<input
-              class="border-4 rounded-lg p-2 w-full border-black text-black"
+              class="
+                border-4
+                rounded-lg
+                p-2
+                w-full
+                border-black
+                text-black
+                focus:outline-none
+              "
               type="text"
               name="name"
               placeholder="DOE"
@@ -31,7 +55,15 @@
             class="text-lg text-center font-medium text-white"
             for="firstName"
             >Prénom<input
-              class="border-4 rounded-lg p-2 w-full border-black text-black"
+              class="
+                border-4
+                rounded-lg
+                p-2
+                w-full
+                border-black
+                text-black
+                focus:outline-none
+              "
               type="text"
               name="firstName"
               placeholder="John"
@@ -42,7 +74,15 @@
         <div class="flex w-full p-2 gap-5 flex-col md:w-1/2">
           <label class="text-lg text-center font-medium text-white" for="email"
             >Email<input
-              class="border-4 rounded-lg p-2 w-full border-black text-black"
+              class="
+                border-4
+                rounded-lg
+                p-2
+                w-full
+                border-black
+                text-black
+                focus:outline-none
+              "
               type="email"
               name="email"
               placeholder="yuldrish@gmail.com"
@@ -53,7 +93,15 @@
             class="text-lg text-center font-medium text-white"
             for="password"
             >Password<input
-              class="border-4 rounded-lg p-2 w-full border-black text-black"
+              class="
+                border-4
+                rounded-lg
+                p-2
+                w-full
+                border-black
+                text-black
+                focus:outline-none
+              "
               type="password"
               name="password"
               placeholder="********"
@@ -64,7 +112,15 @@
             class="text-lg text-center font-medium text-white"
             for="password"
             >Password confirmation<input
-              class="border-4 rounded-lg p-2 w-full border-black text-black"
+              class="
+                border-4
+                rounded-lg
+                p-2
+                w-full
+                border-black
+                text-black
+                focus:outline-none
+              "
               type="password"
               name="password"
               placeholder="********"
@@ -76,7 +132,15 @@
       <div class="flex justify-center">
         <button
           @click="submitForm"
-          class="text-lg font-bold rounded-lg w-1/2 border-black border-4 hover:bg-black hover:text-white sm:w-1/4"
+          class="
+            text-lg
+            font-bold
+            rounded-lg
+            w-1/2
+            border-black border-4
+            hover:bg-black hover:text-white
+            sm:w-1/4
+          "
         >
           Valider <i class="fas fa-user-plus"></i>
         </button>
